b767f7dc498c13b9e8f11f3a8df104ccf18d9907
mysoyul <vega2k_@naver.com>
2024-04-09 16:29:26 +0900

    App.js에서 이벤트핸들러를 Form.js로 이동

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
+    state = {
+        todo: '',
+    }
+    //이벤트핸들러 함수 선언
+    handleChange = (e) => {
+        this.setState({
+            todo: e.target.value // input field의 다음 바뀔 값
+        });
+    }
+    handleCreate = () => {
+        const { todo, todos } = this.state;
+        const newTodo = {
+            id: this.id++,
+            text: todo,
+            checked: false
+        };
+
+        this.setState({
+            todos: [...todos, newTodo],
+            todo: '', // input 초기화
+        });
+    };
+    handleEnter = (e) => {
+        // 눌려진 키가 Enter 이면 handleCreate 호출
+        if (e.keyCode === 13) {
+            this.handleCreate();
+        }
+    };
+
     render() {
         const { todo, myEnter, myChange, myCreate } = this.props;
         return (
