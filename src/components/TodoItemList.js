c7a3bfa079024f4e87be23532bd7def4424b7df2
mysoyul <vega2k_@naver.com>
2024-04-09 10:01:45 +0900

    init commit

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+    render() {
+        const { todos, myToggle, myRemove } = this.props;
+        const todoList = todos.map(
+            ({ id, text, checked }) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={myToggle}
+                    onRemove={myRemove}
+                    key={id}
+                />
+            )
+        );
+        return (
+            <div>
+                {todoList}
+            </div>
+        );
+    }
+}
+export default TodoItemList;
